ae8c6eb724728240e128ae00d24d24e3102e910c
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-11 17:31:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
+
+  def intitialize(name)
+    @name = name
   end
- 
+
   def name
-    @this_dogs_name
+    @name
   end
 end
