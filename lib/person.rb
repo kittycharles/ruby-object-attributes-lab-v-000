fab3a7d60b613c451363db2ef2b8b855eeb82688
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-11 17:57:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -21,30 +21,3 @@ class Person
   end
 
 end
-
-
-
-
-# class Person
-#   def initialize(name, job)
-#     @name=name
-#     @job=job
-#   end
-#
-#   def name
-#     @name
-#   end
-#
-#   def job
-#     @job
-#   end
-#
-#   def name=(new_name)
-#     @name = new_name
-#   end
-#
-#   def job=(new_job)
-#   @job = new_job
-#   end
-#
-# end
