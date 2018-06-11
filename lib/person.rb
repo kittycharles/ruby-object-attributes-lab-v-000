0b6f34620fe8843142c09ecda111f8f8cca52420
Richard Sanchez <richard.charles.s@gmail.com>
2018-06-11 17:45:21 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,4 @@
-# class Person
-#   def initialize(name)
-#     @name = name
-#   end
-#
-#   def name
-#     @name
-#   end
-#
-#   def name= (new_name)
-#     @name = new_name
-#   end
-# end
-
 class Person
-
   def initialize(name)
     @name = name
   end
@@ -22,8 +7,7 @@ class Person
     @name
   end
 
-  def name=(new_name)
+  def name= (new_name)
     @name = new_name
   end
-
 end
